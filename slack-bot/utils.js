931f1d6b382f89af26475f40faf01d606f69ac02
inphood <prabhaav.bhardwaj@gmail.com>
2017-11-09 12:12:21 -0800

    firebase api token passed

--- slack-bot/utils.js
+++ slack-bot/utils.js
@@ -1,28 +1,50 @@
+var fs = require('fs')
+
 exports.initializeFirebase = function(firebaseVar) {
   if (firebaseVar.apps.length === 0) {
-    firebaseVar.initializeApp({
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET
-    })
-    firebaseVar.auth().signInAnonymously()
-    console.log('**************************firebase AUTH')
+    if (!process.env.firebase_token_path) {
+      console.log('Error: Specify slack_token_path in environment')
+      process.exit(1)
+    }
+    fs.readFile(process.env.firebase_token_path, function (err, data) {
+      if (err) {
+        console.log('Error: Specify token in slack_token_path file')
+        process.exit(1)
+      }
+      data = String(data)
+      data = data.replace(/\s/g, '')
+      firebaseVar.initializeApp({
+        apiKey: data,
+        authDomain: 'inphooddb-e0dfd.firebaseapp.com',
+        databaseURL: 'https://inphooddb-e0dfd.firebaseio.com',
+        projectId: 'inphooddb-e0dfd',
+        storageBucket: 'inphooddb-e0dfd.appspot.com'
+      })
+      firebaseVar.auth().signInAnonymously()
+      console.log('**************************firebase AUTH')
+    }
   }
 }
 
 exports.initializeSlack = function(slackControllerVar) {
-  if (!process.env.SLACK_TOKEN) {
+  if (!process.env.slack_token_path) {
     console.log('Error: Specify slack_token_path in environment')
     process.exit(1)
-  } else {
-    slackControllerVar
-    .spawn({token: process.env.SLACK_TOKEN})
-    .startRTM(function (err) {
-      if (err) {
-        throw new Error(err)
-      }
-    })
   }
+
+  fs.readFile(process.env.slack_token_path, function (err, data) {
+    if (err) {
+      console.log('Error: Specify token in slack_token_path file')
+      process.exit(1)
+    }
+    data = String(data)
+    data = data.replace(/\s/g, '')
+    controller
+      .spawn({token: data})
+      .startRTM(function (err) {
+        if (err) {
+          throw new Error(err)
+        }
+      })
+  })
 }
